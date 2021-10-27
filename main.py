35619cb270eb2005bfc09a32793b58d56701e0a5
Tuchsanai <tuchsanai@gmail.com>
2021-10-27 14:00:40 +0700

    a

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/get/")
+def get(a : int, b : Optional[int]=15) :
+    return { 'message': f' a = {a} b= {b}'  }
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
