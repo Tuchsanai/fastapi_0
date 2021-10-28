29b1895a5a03f697e64a1b97e20815d804edcc29
Tuchsanai <tuchsanai@gmail.com>
2021-10-28 15:44:20 +0700

    0

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -16,3 +17,7 @@ def get(a : int, b : Optional[int]=15) :
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get('/blog/type/{type}', tags=['zzz'])
+def get_blog_type(type: int):
+  return {'message': f'Blog type {type}'}    
