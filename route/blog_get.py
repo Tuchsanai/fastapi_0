19bda5b25b888e1d57317667d47dfecdd31b4216
Tuchsanai <tuchsanai@gmail.com>
2021-10-29 18:22:01 +0700

    0

---
+++ route/blog_get.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Response, status
+from enum import Enum
+from typing import Optional
+
+routerget = APIRouter(prefix='/blog_get', tags=['blog'])
+
+
+@routerget.get('/all', summary='Retrieve all blogs',  description='This api call simulates fetching all blogs',
+  response_description="The list of available blogs"
+  )
+def get_blogs(page = 1, page_size: Optional[int] = None):
+  return {'message': f'All {page_size} blogs on page {page}'}
