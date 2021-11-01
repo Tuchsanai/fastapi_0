f13441f0499c083066dd417bd1bee1bab05aae98
tuchsanai <tuchsanai@gmail.com>
2021-11-01 21:47:38 +0700

    0

--- router/blog_get.py
+++ router/blog_get.py
@@ -1,13 +1,19 @@
 from fastapi import APIRouter, Response, status
 from enum import Enum
 from typing import Optional
+from pydantic import BaseModel
+
 
 routerget = APIRouter(prefix='/blog_get', tags=['blog'])
 
+class BlogModel(BaseModel):
+    url: str
+    alias: str
 
-@routerget.get('/data')
-def get1(page = 1, page_size: Optional[int] = None):
-  return {'message': f'All {page_size} blogs on page {page}'}
+
+@routerget.get('/bdata')
+def get2(blog: BlogModel ,page = 1, page_size: Optional[int] = None):
+  return {'blog':blog,'message': f'All {page_size} blogs on page {page}'}
 
 
 
